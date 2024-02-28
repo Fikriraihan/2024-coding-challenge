2f81c6014c5990ba38821be9957e1e3634d57223
Fikriraihan <fikri.raihan4@gmail.com>
2024-02-28 20:22:27 +0700

    multiply & even or odd (day 5)

---
+++ evenOrOdd.js
@@ -0,0 +1,15 @@
+// Coding Challenge 2024 5/366
+// Even or Odd
+// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+
+// function evenOrOdd(number) {
+//   if (number % 2 === 0) {
+//     return "Even";
+//   } else {
+//     return "Odd";
+//   }
+// }
+
+const evenOrOdd = (number) => (number % 2 === 0 ? "even" : "odd");
+
+console.log(evenOrOdd(2));
---
+++ multiply.js
@@ -0,0 +1,11 @@
+// Coding Challenge 2024 5/366
+// Even or Odd
+// https://www.codewars.com/kata/50654ddff44f800200000004
+
+// function multiply(a, b) {
+//   return a * b;
+// }
+
+const multiply = (a, b) => a * b;
+
+console.log(multiply(0, 2));
